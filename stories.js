08de04da71062daed763f56e2d173b98df5f198a
shawn <shawn@naturalprofessional.com>
2016-09-18 09:49:19 -0700

    Comment out unused js files

--- stories.js
+++ stories.js
@@ -1,44 +1,44 @@
-function sendQuickEmotion(recipientId) {
-  var messageData = {
-    recipient: {
-      id: recipientId
-    },
-    message: {
-      text: "Which emoji represents your current emotion?",
-      metadata: "DEVELOPER_DEFINED_METADATA",
-      quick_replies: [
-        {
-          "content_type":"text",
-          "title":"😡",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
-        },
-        {
-          "content_type":"text",
-          "title":"😂",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"😒",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
-        },
-        {
-          "content_type":"text",
-          "title":"😭",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"😊",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-      ]
-    }
-  };
-
-  callSendAPI(messageData);
-}
-
-module.exports = {
-  sendQuickEmotion: sendQuickEmotion
-};
+// function sendQuickEmotion(recipientId) {
+//   var messageData = {
+//     recipient: {
+//       id: recipientId
+//     },
+//     message: {
+//       text: "Which emoji represents your current emotion?",
+//       metadata: "DEVELOPER_DEFINED_METADATA",
+//       quick_replies: [
+//         {
+//           "content_type":"text",
+//           "title":"😡",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
+//         },
+//         {
+//           "content_type":"text",
+//           "title":"😂",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+//         },
+//         {
+//           "content_type":"text",
+//           "title":"😒",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+//         },
+//         {
+//           "content_type":"text",
+//           "title":"😭",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+//         },
+//         {
+//           "content_type":"text",
+//           "title":"😊",
+//           "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+//         },
+//       ]
+//     }
+//   };
+//
+//   callSendAPI(messageData);
+// }
+//
+// module.exports = {
+//   sendQuickEmotion: sendQuickEmotion
+// };
