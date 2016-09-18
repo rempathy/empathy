c6d28e565014c8b61b03ebce92c133ac2326cda7
Sean Lee <seanboramlee@gmail.com>
2016-09-17 21:07:36 -0700

    rename all helpers

--- helper_functions.js
+++ helper_functions.js
@@ -78,9 +78,10 @@ var getQuote = function(card) {
 
 module.exports = {
   emotions: emotions,
-  accessEmotion: accessEmotion,
-  chooseRemedy: chooseRemedy,
-  accessCard: accessCard,
+  randomRemedy: randomRemedy,
+  getCard: getCard,
+  getIntro: getIntro,
   getRemedy: getRemedy,
-  getMoreInfo: getMoreInfo
+  getMoreInfo: getMoreInfo,
+  getQuote: getQuote
 };
