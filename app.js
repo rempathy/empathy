19ce25edae2aa3e5916d88fc02a1166d9752604a
Chris Bassano <christo4b@gmail.com>
2016-09-18 10:58:03 -0700

    changed payloads to reflect emoji

--- app.js
+++ app.js
@@ -812,27 +812,17 @@ function sendQuickEmotion(recipientId) {
         {
           "content_type":"text",
           "title":"😡",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ACTION"
-        },
-        {
-          "content_type":"text",
-          "title":"😂",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
-        },
-        {
-          "content_type":"text",
-          "title":"😒",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_DRAMA"
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_ANGER"
         },
         {
           "content_type":"text",
           "title":"😭",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_SAD"
         },
         {
           "content_type":"text",
           "title":"😊",
-          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_COMEDY"
+          "payload":"DEVELOPER_DEFINED_PAYLOAD_FOR_PICKING_HAPPY"
         },
       ]
     }
@@ -967,7 +957,7 @@ function sendInitialResponse(recipientID){
  *
  */
 function callSendAPI(messageData) {
-  console.log("IN CALLSENDAPI !!!");
+  
   request({
     uri: 'https://graph.facebook.com/v2.6/me/messages',
     qs: { access_token: PAGE_ACCESS_TOKEN },
@@ -975,11 +965,11 @@ function callSendAPI(messageData) {
     json: messageData
 
   }, function (error, response, body) {
-    console.log("IN CALLSENDAPI CALLBACK!!!");
+    
     if (!error && response.statusCode == 200) {
       var recipientId = body.recipient_id;
       var messageId = body.message_id;
-      console.log("IN IF BLOCK!!!")
+      
       if (messageId) {
         console.log("Successfully sent message with id %s to recipient %s", 
           messageId, recipientId);
@@ -988,9 +978,8 @@ function callSendAPI(messageData) {
         recipientId);
       }
     } else {
-      // console.log("IN ELSE BLOCK!!! ")
-      // console.log("RESPONSE IS ", response);
       console.error(response.error);
+      console.log("Response Error is:", response.error);
     }
   });  
 }
